9d79f03a13c1668eb886b4f4b6bd14db904e05b5
Code-ga <tritranduc11@gmail.com>
2026-05-01 13:45:15 +0700

    can we try new env solution

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,7 +1,7 @@
 import { logger } from "./lib/logger";
 export const BACKEND_URL =
-	process.env.NEXT_BACKEND_URL || "http://localhost:3001";
+	import.meta.env.NEXT_BACKEND_URL || "http://localhost:3001";
 export const FRONTEND_URL =
-	process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
-logger.info(BACKEND_URL);
-logger.info(FRONTEND_URL);
+	import.meta.env.NEXT_PUBLIC_URL || "http://localhost:3000";
+logger.info({ env: import.meta.env, processEnv: process.env });
+logger.info({ BACKEND_URL, FRONTEND_URL });
